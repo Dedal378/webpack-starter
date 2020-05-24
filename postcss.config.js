b611a76d2b5dedd7e6ce9cfcafe07eea91b67408
Alex <41344035+Dedal378@users.noreply.github.com>
2020-05-24 14:33:07 +0300

    add postcss config

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+
+// autoprefixer - https://github.com/postcss/autoprefixer
+// css-mqpacker - https://github.com/hail2u/node-css-mqpacker
+// cssnano      - https://github.com/hail2u/node-css-mqpacker
+
+// npm i postcss-loader autoprefixer css-mqpacker cssnano -D
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    require('css-mqpacker'),
+    require('cssnano')({
+      preset: [
+        'default', {
+          discardComments: {
+            removeAll: true
+          }
+        }
+      ]
+    })
+  ]
+};
